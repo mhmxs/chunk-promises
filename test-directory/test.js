393070f59d29f9b3bd4aee79ee3e5bd0bd91b72a
mhmxs <kovacsricsi@gmail.com>
2019-09-25 15:29:47 +0200

    Update documentation

--- test-directory/test.js
+++ test-directory/test.js
@@ -5,18 +5,18 @@ const chunkPromises = require('../index.js').chunkPromises;
 describe('Test chunked promise execution:', function() {
   it('all 4 chunks must execute', function() {
     var promises = [
-      new Promise(resolver => { resolver("a") }),
-      new Promise(resolver => { resolver("b") }),
-      new Promise(resolver => { resolver("c") }),
-      new Promise(resolver => { resolver("d") }),
-      new Promise(resolver => { resolver("e") }),
-      new Promise(resolver => { resolver("f") }),
-      new Promise(resolver => { resolver("g") }),
+      () => new Promise(resolver => { resolver("a") }),
+      () => new Promise(resolver => { resolver("b") }),
+      () => new Promise(resolver => { resolver("c") }),
+      () => new Promise(resolver => { resolver("d") }),
+      () => new Promise(resolver => { resolver("e") }),
+      () => new Promise(resolver => { resolver("f") }),
+      () => new Promise(resolver => { resolver("g") }),
     ]
 
     chunkPromises(promises, 2)
       .then(results => {
-        assert(["a", "b", "c", "d", "e", "f", "g"] === results);
+        assert(7 === results.length);
       });
   });
 });
