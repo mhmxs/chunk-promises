827466abc85618a0a35e1855780800f0d29984ef
Richard Kovacs <kovacsricsi@gmail.com>
2021-11-02 11:15:36 +0100

    Format source code

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 deps:
 	npm ci
 
+fmt:
+	./node_modules/prettier/bin-prettier.js -w index.js
+
 test:
 	./node_modules/.bin/mocha test-directory/test.js
 
--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-'use strict';
+"use strict";
 
-exports.chunkPromises = function(promises, size) {
-    if (!Array.isArray(promises) || !promises.length) {
-        return Promise.resolve([])
-    }
+exports.chunkPromises = function (promises, size) {
+  if (!Array.isArray(promises) || !promises.length) {
+    return Promise.resolve([]);
+  }
 
-    var size = size ? size : 10;
+  size = size ? size : 10;
 
-    var chunks = [];
-    for (var i = 0, j = promises.length; i < j; i += size) {
-        chunks.push(promises.slice(i, i + size));
-    }
+  var chunks = [];
+  for (var i = 0, j = promises.length; i < j; i += size) {
+    chunks.push(promises.slice(i, i + size));
+  }
 
-    var collector = Promise.resolve([]);
-    chunks.forEach(chunk => {
-      collector = collector.then(results => Promise.all(chunk.map(c => c()))
-        .then(subResults => results.concat(subResults)));
-    });
-    return collector;
-}
+  var collector = Promise.resolve([]);
+  chunks.forEach((chunk) => {
+    collector = collector.then((results) =>
+      Promise.all(chunk.map((c) => c())).then((subResults) =>
+        results.concat(subResults)
+      )
+    );
+  });
+  return collector;
+};
